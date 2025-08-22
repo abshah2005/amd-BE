f16c58a70e466f9e3ff85e86c858a76226800e00
abshah2005 <abdullah03350904415@gmail.com>
2025-08-22 23:31:09 +0500

    made a small change

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ import { startAgenda } from "./jobs/agenda/AgendaScheduler.js";
 const app = express();
 
 app.use(cors({
-  origin: ["https://ask-me-direct-lgm26fj9h-adeems-projects.vercel.app","*"],
+  origin:[process.env.FRONTEND_URL],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
