7b9cd23d690ca707a577c01472dcbcf494e5d7b5
abshah2005 <abdullah03350904415@gmail.com>
2026-06-30 19:41:57 +0500

    added a new job

--- src/app.js
+++ src/app.js
@@ -36,6 +36,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.use("/api/users", userRoute);
 app.use("/api/users/payments", paymentRoute);
 app.use("/api/specializations", specializationRoute);
